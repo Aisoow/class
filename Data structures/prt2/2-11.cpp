6d96a413db214d0d8a8a62f92fa55fa12f1f4d4b
Aisoow <aisoow0w0@gmail.com>
2025-10-14 22:10:06 +0800

    prt

---
+++ Data structures/prt2/2-11.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+const int a_rows = 4;
+const int a_columns = 3;
+const int b_rows = 100;
+const int b_columns = 3;
+
+void compress_init(int a[][a_columns])
+{
+    for (int i = 0; i < a_rows; i++)
+        for (int j = 0; j < a_columns; j++)
+            cin >> a[i][j];
+}
+void compress_process(int a[][a_columns], int b[][b_columns])
+{
+    int c = 0;
+    for (int i = 0; i < a_rows; i++)
+        for (int j = 0; j < a_columns; j++)
+            if (a[i][j] != 0)
+                c++;
+    b[0][0] = a_rows;
+    b[0][1] = a_columns;
+    b[0][2] = c;
+    int n = 1;
+    for (int i = 0; i < a_rows; i++)
+        for (int j = 0; j < a_columns; j++)
+        {
+            if (a[i][j] != 0)
+            {
+                b[n][0] = i;
+                b[n][1] = j;
+                b[n][2] = a[i][j];
+                n++;
+            }
+        }
+}
+void compress_print(int b[][b_columns])
+{
+    for (int i = 0; i < b[0][2] + 1; i++)
+    {
+        for (int j = 0; j < b_columns; j++)
+        {
+            cout << b[i][j];
+        }
+    }
+}
+int main(int argc, char *argv[])
+{
+    int A[a_rows][a_columns], B[b_rows][b_columns];
+    compress_init(A);
+    compress_process(A, B);
+    compress_print(B);
+}
